f76f684d5afa8232701845ae0d7e3bc754fc1b54
fillip1984 <fillip1984@gmail.com>
2014-04-07 22:53:23 -0400

    Added a test class so that the test/java directory has something

---
+++ petclinic2/src/test/java/org/home/petclinic2/validator/PetValidatorTest.java
@@ -0,0 +1,19 @@
+package org.home.petclinic2.validator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PetValidatorTest {
+
+	@Test
+	public void testSupports() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidate() {
+		fail("Not yet implemented");
+	}
+
+}
