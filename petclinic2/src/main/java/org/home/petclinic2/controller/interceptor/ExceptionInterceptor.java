34ba8baa3649921f52b7727c4cec4418f7ffdc13
fillip1984 <fillip1984@gmail.com>
2014-04-14 01:39:26 -0400

    while implementing an example HandlerInterceptor that I've used previously to email support I came across the concept of ExceptionResolvers which go one step further and send the user to a view showing them a friendly "Error occurred" screen. I kept this code in the code base just to show an alternative that I've used

--- petclinic2/src/main/java/org/home/petclinic2/controller/interceptor/ExceptionInterceptor.java
+++ petclinic2/src/main/java/org/home/petclinic2/controller/interceptor/ExceptionInterceptor.java
@@ -1,5 +1,8 @@
 package org.home.petclinic2.controller.interceptor;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,7 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jmx.export.notification.UnableToSendNotificationException;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,16 +20,38 @@ import org.springframework.web.servlet.ModelAndView;
  * <p>
  * Except for when the exception is that we couldn't send an email
  * 
+ * @deprecated Was using this class to send errors in application to the support
+ *             team, but I think a HandlerExceptionResolver will solve the issue
+ *             better. It will still need to email support, but it will also
+ *             send the user to a more friendly "Error has occurred" page
+ *             instead of letting the container use its default
  * @author phil
  * 
  */
-@Component
+// @Component
+@Deprecated()
 public class ExceptionInterceptor implements HandlerInterceptor {
 	private static final Logger logger = LoggerFactory
 			.getLogger(ExceptionInterceptor.class);
 
 	@Autowired
 	private EmailService emailService;
+
+	private List<String> supportEmailAddresses;
+
+	/**
+	 * Ensure that supportEmailAddresses was initialized
+	 */
+	// @PostConstruct is really helpful for ensuring that components are
+	// initialized properly. You will notice that constructors are hardly relied
+	// upon
+	@PostConstruct
+	public void initEmailSupportSimpleMappingExceptionResolver() {
+		if (supportEmailAddresses == null || supportEmailAddresses.isEmpty()) {
+			throw new IllegalStateException(
+					"Must supply at least one email address so that we can email support when exceptions occur in the application");
+		}
+	}
 
 	@Override
 	public void afterCompletion(HttpServletRequest request,
@@ -40,7 +64,8 @@ public class ExceptionInterceptor implements HandlerInterceptor {
 			// to get in an infinite loop so we only log these sort of
 			// exceptions
 			if (!(ex instanceof UnableToSendNotificationException)) {
-				emailService.sendExceptionToSupport(ex, MDC.get("userName"));
+				emailService.sendExceptionToSupport(ex, MDC.get("userName"),
+						supportEmailAddresses);
 			} else {
 				// when the exception is that we couldn't send an exception to
 				// support we just log it
@@ -62,6 +87,14 @@ public class ExceptionInterceptor implements HandlerInterceptor {
 			HttpServletResponse response, Object handler) throws Exception {
 		// doing nothing
 		return true;
+	}
+
+	public List<String> getSupportEmailAddresses() {
+		return supportEmailAddresses;
+	}
+
+	public void setSupportEmailAddresses(List<String> supportEmailAddresses) {
+		this.supportEmailAddresses = supportEmailAddresses;
 	}
 
 }
