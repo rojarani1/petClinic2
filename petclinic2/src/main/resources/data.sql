1220aea12a21e4fd0da2785e264f540929dac18a
fillip1984 <fillip1984@gmail.com>
2014-04-14 01:19:53 -0400

    came up with data.sql to preload the db with a user so that we have a system account

--- petclinic2/src/main/resources/data.sql
+++ petclinic2/src/main/resources/data.sql
@@ -1,5 +1,2 @@
-insert into user(id,email,password,firstname,lastname) values (0,'rob@example.com','password','Rob','Winch');
-insert into user(id,email,password,firstname,lastname) values (1,'luke@example.com','password','Luke','Taylor');
-
-insert into message(id,created,to_id,summary,text) values (100,'2013-10-04 10:00:00',0,'Hello Rob','This message is for Rob');
-insert into message(id,created,to_id,summary,text) values (110,'2013-10-04 10:00:00',1,'Hello Luke','This message is for Luke');
+INSERT INTO USER
+VALUES (0, {ts '2014-04-14 00:00:00.000'}, {ts '2014-04-14 00:00:00.000'}, 'SYSTEM', 'ACCOUNT', '5555555555', 'system.account@noWhere.com', '12345', 0, 0, NULL);
